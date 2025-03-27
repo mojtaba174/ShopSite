fb1b58fdf56cfe03d6496ee970b38d1e3e475d18
alisalsali <alisalslio@gnmail.com>
2025-03-27 12:18:57 +0330

    ok

--- comments/views.py
+++ comments/views.py
@@ -31,30 +31,3 @@ def add_comment(request, product_slug):
     form = CommentForm()
     context = {'form': form, 'comments': comments, 'product': product}
     return render(request, 'shop/detail_page.html', context)
-
-# from django.shortcuts import get_object_or_404, redirect ,render
-# from django.contrib import messages
-# from shop.models import Product
-# from .models import Comment
-# from django.contrib.auth.decorators import login_required
-# from .forms import CommentForm
-#
-#
-# @login_required
-# def add_comment(request, product_slug):
-#     product = get_object_or_404(Product, slug=product_slug)
-#     if request.method == 'POST':
-#         form = CommentForm(request.POST)
-#         if form.is_valid():
-#             comment = form.save(commit=False)
-#             comment.user = request.user
-#             comment.product = product
-#             comment.save()
-#             messages.success(request, "نظر شما با موفقیت ثبت شد.")
-#             return redirect('shop:detail_page', slug=product_slug)
-#     else:
-#         form = CommentForm()
-#
-#     comments = product.comments.filter(approved=True).order_by('-created_at')
-#     context = {'form': form, 'comments': comments, 'product': product}
-#     return render(request, 'shop/detail_page.html', context)
