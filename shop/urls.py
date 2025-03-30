abd8ba723ea0057c5ab30bfa5766ce3884584ba8
mojtab1_0 <mojtabaansari.83.01@gmail.com>
2025-03-30 15:20:35 +0330

    create about us and contact us

--- shop/urls.py
+++ shop/urls.py
@@ -4,13 +4,15 @@ from . import views
 app_name = 'shop'
 urlpatterns = [
     path('', views.home_page, name='home_page'),
-    path('shop/product/<slug:slug>', views.detail_page, name='detail_page'),
-    path('shop/cart', views.cart_page, name='cart_page'),
-    path('shop/add_to_cart/<slug:slug>', views.add_to_cart, name='add_to_cart'),
-    path('shop/increase_cart_quantity/<slug:slug>/<int:variant_id>', views.increase_cart_quantity,
+    path('shop/product/<slug:slug>/', views.detail_page, name='detail_page'),
+    path('shop/cart/', views.cart_page, name='cart_page'),
+    path('shop/add_to_cart/<slug:slug>/', views.add_to_cart, name='add_to_cart'),
+    path('shop/increase_cart_quantity/<slug:slug>/<int:variant_id>/', views.increase_cart_quantity,
          name='increase_cart_quantity'),
-    path('shop/decrease_cart_quantity/<slug:slug>/<int:variant_id>', views.decrease_cart_quantity,
+    path('shop/decrease_cart_quantity/<slug:slug>/<int:variant_id>/', views.decrease_cart_quantity,
          name='decrease_cart_quantity'),
-    path('shop/filter_category/<slug:slug>', views.filter_category, name='filter_category'),
-    path('shop/search_results', views.search_results, name='search_results')
+    path('shop/filter_category/<slug:slug>/', views.filter_category, name='filter_category'),
+    path('shop/search_results/', views.search_results, name='search_results'),
+    path('about_us/', views.about_us, name='about_us'),
+    path('contact_us/', views.contact_us, name='contact_us'),
 ]
