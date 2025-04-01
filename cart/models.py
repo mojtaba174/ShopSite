592a42d73c24db3aa2618101fcf9c42cef7501e6
mojtab1_0 <mojtabaansari.83.01@gmail.com>
2025-04-01 08:21:51 +0330

    edit cart app

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+from shop.models import Product, ProductVariant
 
-# Create your models here.
+
+class CartItem(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    product_variant = models.ForeignKey(ProductVariant, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.product.title} ({self.quantity})"
